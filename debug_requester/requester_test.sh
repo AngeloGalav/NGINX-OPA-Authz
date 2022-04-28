714a9ff5b1e4d0fe5bd923dc5c69e7ce803c178a
AngeloGalav <tekken342@gmail.com>
2022-04-28 16:34:14 +0200

    fixed cacert, changed to openresty

--- debug_requester/requester_test.sh
+++ debug_requester/requester_test.sh
@@ -18,4 +18,4 @@ else
     role="$2"
 fi
 
-curl --cacert "../nginx_docker_revproxy/certificates_for_https/star.test.example.cert.pem" -X "POST" "https://pippocnaf.test.example:8081/operation/" -H "X-Role: $1" -H "X-Operation: $2" -H "X-EnableJWT: false" 
+curl --cacert "../nginx_docker_revproxy/certificates_for_https/igi-test-ca.pem" -X "POST" "https://pippocnaf.test.example:8081/operation/" -H "X-Role: $1" -H "X-Operation: $2" -H "X-EnableJWT: false" 
