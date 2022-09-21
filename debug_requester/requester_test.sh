f51af50e6c51b80db6b896c4f99cf1f0fa87c267
AngeloGalav <tekken342@gmail.com>
2022-09-21 17:54:41 +0200

    Fix requester

--- debug_requester/requester_test.sh
+++ debug_requester/requester_test.sh
@@ -5,23 +5,21 @@ role="";
 operation=""
 
 if [ -z "$1" ]; then
-    echo "you must input an operation for this to work"
+    echo "you must input a role for this to work (or enable the JWT)"
     exit 1
 else 
-    operation="$1"
+    role="$1"
 fi
 
 if [ "$2" == "-t" ] || [ -z "$2" ]; then
-     echo "you must input a role for this to work (or enable the JWT)"
-     exit 1
+    echo "you must input an operation for this to work"
+    exit 1
 else
-    role="$2"
+    operation="$2"
 fi
 
-if [ operation == "image_request" ]; then 
-     curl --cacert "../nginx_docker_revproxy/certificates_for_https/igi-test-ca.pem" "https://servicecnaf.test.example:8081/operation/image_request" -H "X-Role: $role" -H "X-Operation: image_request" -H "X-EnableJWT: false" --output test.jpg
+if [ "$operation" = "image_request" ]; then 
+    curl --cacert "./nginx_docker_revproxy/certificates_for_https/igi-test-ca.pem" "https://servicecnaf.test.example:8081/operation/image_request" -H "X-Role: $role" -H "X-Operation: image_request" -H "X-EnableJWT: false" --output test.jpg
 else
-
+    curl -Li --cacert "./nginx_docker_revproxy/certificates_for_https/igi-test-ca.pem" -X "POST" "https://servicecnaf.test.example:8081/operation/" -H "X-Role: $role" -H "X-Operation: $operation" -H "X-EnableJWT: false" 
 fi
-
-curl --cacert "../nginx_docker_revproxy/certificates_for_https/igi-test-ca.pem" -X "POST" "https://servicecnaf.test.example:8081/operation/" -H "X-Role: $role" -H "X-Operation: $operation" -H "X-EnableJWT: false" 
